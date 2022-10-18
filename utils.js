d0821bada98b402af3bfed15cea2e8ff5203eb3f
Bibek Shah <90471455+bibekshhh@users.noreply.github.com>
2022-10-18 10:36:56 +0545

    Update utils.js
    removed confidential login details

--- utils.js
+++ utils.js
@@ -10,8 +10,8 @@ const CONFIRM_TRANSACTION =
 async function login() {
   try {
     const credentials = {
-      password: "@Bibek8848",
-      username: "9827314543",
+      password: <your password>,
+      username: <your username>,
     };
 
     const options = {
@@ -38,9 +38,9 @@ async function initiateTransation(token) {
     token = `Bearer ${token}`;
 
     let payload = {
-      amount: "0.2",
+      amount: <transfer amount>,
       message: "test",
-      receiver: "9806085429",
+      receiver: <reciever username>,
     };
 
     const options = {
@@ -71,7 +71,7 @@ async function confirmTransaction(token, __txn_id__) {
         "content-type": "application/json",
         authorization: token,
       },
-      body: JSON.stringify({ __txn_id__, pin_code: "1104" }),
+      body: JSON.stringify({ __txn_id__, pin_code: <your transaction pin> }),
     };
 
     const res = await fetch(CONFIRM_TRANSACTION, options);
